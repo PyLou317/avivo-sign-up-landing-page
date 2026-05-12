c0f207e178b8a3a28bc87fe546f65ba8b821806d
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-05-11 21:45:35 -0700

    add hand writting font

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        handwriting: ['"Just Another Hand"', 'cursive'],
+      },
+      keyframes: {
+        shimmer: {
+          '100%': { transform: 'translateX(100%)' },
+        },
+      },
+    },
+  },
+};
