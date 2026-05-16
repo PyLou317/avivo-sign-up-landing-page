14aa95d99dc75730646747126fc3b25fcdc582de
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-05-15 20:09:58 -0700

    update with scroll to top

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router';
-
-import GuestsLearnMore from './pages/guests/GuestLearnMore.jsx';
-import RestaurantsLearnMore from './pages/restaurants/RestaurantsLearnMore.jsx';
+import { BrowserRouter } from 'react-router';
+import ScrollToTop from './utils/ScrollToTop';
 
 import './index.css';
-import App from './App.jsx';
+import Route from './Root.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-        <Route path="guests" element={<GuestsLearnMore />} />
-        <Route path="restaurants" element={<RestaurantsLearnMore />} />
-      </Routes>
+      <ScrollToTop />
+      <Route />
     </BrowserRouter>
     ,
   </StrictMode>,
