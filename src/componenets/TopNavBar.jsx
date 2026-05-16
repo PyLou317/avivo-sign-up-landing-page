77fa8110569b9b0709866c4fe7a2fc836a3ff5fc
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-05-15 21:46:11 -0700

    style: unhide on scroll

--- src/componenets/TopNavBar.jsx
+++ src/componenets/TopNavBar.jsx
@@ -1,27 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { NavLink } from 'react-router';
-import ScrollToTop from '../utils/ScrollToTop';
-import ActiveTabToggle from './ActiveTabToggle';
-import AreYouARestaurant from './AreYouARestaurant';
+import ScrollToTop from '../../src/utils/ScrollToTop';
 
-const Navbar = ({ activeTab, setActiveTab }) => {
-  const [isOpen, setIsOpen] = useState(false);
+const Navbar = () => {
+  const [hidden, setHidden] = useState();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Show navbar if scrolled down more than 20px
+      if (window.scrollY > 20) {
+        setHidden(true);
+      } else {
+        setHidden(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <>
-      <nav className="fixed top-0 w-full z-50 bg-white/80 backdrop-blur-md border-b border-gray-100">
+      <nav
+        className={`fixed top-0 w-full z-50 bg-white/80 backdrop-blur-md border-b border-gray-100 transition-transform duration-300 ease-in-out ${hidden ? 'translate-y-0' : '-translate-y-full'}`}
+      >
         <div className="max-w-7xl mx-auto px-6 h-16 flex items-center justify-between">
           {/* Logo */}
-          <NavLink to="/">
+          <NavLink to="/" onClick={ScrollToTop}>
             <div className="text-xl font-bold tracking-tighter">AVIVO</div>
           </NavLink>
-
-          <div className="relative flex items-center">
-            <ActiveTabToggle
-              activeTab={activeTab}
-              setActiveTab={setActiveTab}
-            />
-          </div>
         </div>
       </nav>
     </>
