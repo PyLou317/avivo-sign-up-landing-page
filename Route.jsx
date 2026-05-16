5e4d35f5237a20b90276215932e60cd5d03adf9a
Lucas Patriquin <lucas.patriquin@gmail.com>
2026-05-15 21:06:18 -0700

    add route.jsx

---
+++ Route.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router';
+import Navbar from './componenets/TopNavBar.jsx';
+import App from './App.jsx';
+import GuestsLearnMore from './pages/guests/GuestLearnMore';
+import RestaurantsLearnMore from './pages/restaurants/RestaurantsLearnMore';
+
+function Root() {
+  const [activeTab, setActiveTab] = useState('Guest');
+
+  return (
+    <>
+      <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
+
+      <main className="pt-10">
+        <Routes>
+          <Route
+            index
+            element={<App activeTab={activeTab} setActiveTab={setActiveTab} />}
+          />
+          <Route
+            path="guests"
+            element={
+              <GuestsLearnMore
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+            }
+          />
+          <Route
+            path="restaurants"
+            element={
+              <RestaurantsLearnMore
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+            }
+          />
+        </Routes>
+      </main>
+    </>
+  );
+}
+
+export default Root;
